bc4c2da0e687817a4e078a53a20f7312820d452e
AustinEBailey <79767912+AustinEBailey@users.noreply.github.com>
2023-06-09 09:22:56 -0400

    Added image of enemy

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,14 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 300, 1); 
+        prepare();
+        addObject(new BasicEnemy(),180,200) ;
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
