36e44d330e9e449b89b19b4cfe826743c82dc550
AustinEBailey <79767912+AustinEBailey@users.noreply.github.com>
2023-06-13 17:50:31 -0400

    added a way to kill enemy

---
+++ Amo.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Amo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Amo extends Actor
+{
+    /**
+     * Act - do whatever the Amo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        mapMove();
+        // Add your action code here.
+    }
+     public void mapMove()
+    {
+        if(Greenfoot.isKeyDown("right"))
+        {
+            setLocation(getX() -4, getY());
+        }
+        if(Greenfoot.isKeyDown("left"))
+        {
+            setLocation(getX() + 4, getY());
+        }
+    }
+}
