983858c1e585c46e23867a3a5d7b37f7519a1b30
AustinEBailey <79767912+AustinEBailey@users.noreply.github.com>
2023-06-13 09:56:43 -0400

    I think I fixed ground

---
+++ Ground.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Coin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ground extends Actor
+{
+    /**
+     * Act - do whatever the Coin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        //keep
+        mapMove();
+        // Add your action code here.
+    }
+     public void mapMove()
+    {
+        //keep
+        if(Greenfoot.isKeyDown("right"))
+        {
+            setLocation(getX() + -6, getY());
+        }
+        if(Greenfoot.isKeyDown("left"))
+        {
+            setLocation(getX() + 6, getY());
+        }
+    }
+}
+
